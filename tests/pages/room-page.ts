deefd6d97f94cc6939e1256ad84ebcdeb890afde
Schania Karadaghi <schania_karadaghi@hotmail.com>
2024-09-09 12:34:00 +0200

    Rooms done

--- tests/pages/room-page.ts
+++ tests/pages/room-page.ts
@@ -14,21 +14,21 @@ export class CreateRoomPage {
 
     constructor(page: Page) {
         this.page = page;
-        this.categoryElement = page.getByRole('combobox');
-        this.numberInput = page.locator('input[name="number"]'); // Example selector
-        this.floorInput = page.locator('select[name="floor"]'); // Example selector
-        this.availableCheckbox = page.locator('input[name="available"]'); // Example selector
-        this.priceInput = page.locator('input[name="price"]'); // Example selector
-        this.featuresSelect = page.getByRole('listbox', { name: 'Features' }); // Funktioner (Ctrl/Cmd + klick)
-        this.saveButton = page.getByRole('button', { name: 'Save' }); // Example selector
+        this.categoryElement = page.locator('#app > div > div:nth-child(2) > div:nth-child(1) > select');
+        this.numberInput = page.locator('#app > div > div:nth-child(2) > div:nth-child(2) > input[type=number]'); // Example selector
+        this.floorInput = page.locator('#app > div > div:nth-child(2) > div:nth-child(3) > input[type=number]'); // Example selector
+        this.availableCheckbox = page.locator('#app > div > div:nth-child(2) > div:nth-child(4) > div'); // Example selector
+        this.priceInput = page.locator('#app > div > div:nth-child(2) > div:nth-child(5) > input[type=number]');
+        this.featuresSelect = page.locator('#app > div > div:nth-child(2) > div:nth-child(6) > select'); // Funktioner (Ctrl/Cmd + klick)
+        this.saveButton = page.locator('#app > div > div.actions > a.btn.blue'); // Example selector
      }
 
         async fillOutCreateRoomsForm() {
             const number = faker.number.int({ min: 100, max: 999 }).toString();
             const floorNumber = faker.number.int({ min: 0, max: 10}).toString()
-            const roomPrice = faker.finance.amount({ min: 100000, max: 50000000});
+            const roomPrice = faker.number.int({ min: 100000, max: 50000000 }).toString();
             const features = ['Balcony', 'Ensuite', 'Sea View', 'Penthouse'];
-            const selectedFeatures = features.filter(() => Math.random() > 0.5); // Slumpmässigt välj 2-3 funktioner
+            const selectedFeatures = features.filter(() => Math.random()); // Slumpmässigt välj 2-3 funktioner
             for (const feature of selectedFeatures) {
             await this.featuresSelect.selectOption({ label: feature });
     }
@@ -37,9 +37,8 @@ export class CreateRoomPage {
             await this.categoryElement.selectOption({ index: 1 }); // Selecting single room option
             await this.numberInput.fill(number);
             await this.floorInput.fill(number);
-            await this.availableCheckbox.check();
+            await this.availableCheckbox.click();
             await this.priceInput.fill(roomPrice);
-        
             await this.saveButton.click();
           }
           
